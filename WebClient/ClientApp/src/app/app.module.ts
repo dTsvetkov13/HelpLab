51ba4de6a98cab5837640c5b7ee84340608ec934
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-09 17:46:20 +0200

    New components added

--- WebClient/ClientApp/src/app/app.module.ts
+++ WebClient/ClientApp/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PostComponent } from './pages/post/post.component';
-import { CreatePostComponent } from './pages/create-post/create-post.component';
+import { CreatePostComponent } from './pages/create-post-form/create-post.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,8 @@ import { CreatePostComponent } from './pages/create-post/create-post.component';
     HomeComponent,
     PostComponent,
     CreatePostComponent,
+    LoginComponent,
+    SignUpComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,7 +31,11 @@ import { CreatePostComponent } from './pages/create-post/create-post.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'home', component: HomeComponent, pathMatch: 'full' },
       { path: 'post', component: PostComponent, pathMatch: 'full' },
+      { path: 'login', component: LoginComponent, pathMatch: 'full'},
+      { path: 'post/create', component: CreatePostComponent, pathMatch: 'full'},
+      { path: 'signup', component: SignUpComponent, pathMatch: 'full'}
     ]),
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
