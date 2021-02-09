5270261dc1ef0b9528bbf03db19f55e1a0013f78
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-09 17:52:20 +0200

    Search class init

---
+++ WebClient/ClientApp/src/app/models/search.ts
@@ -0,0 +1,11 @@
+export class Search {
+
+    public question: string;
+    public publisher: string;
+    public subject: string;
+    public grade: string;
+    public page: string;
+    public exercise: string;
+
+    constructor() {  }
+}
