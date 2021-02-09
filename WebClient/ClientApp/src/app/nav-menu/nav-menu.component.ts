b0861a7498e746ec24b00cc1a5e253c685a12f46
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-09 17:33:42 +0200

    Nav bar updated, login and sign up buttons added

--- WebClient/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ WebClient/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Search } from '../models/search';
 
 @Component({
   selector: 'app-nav-menu',
@@ -10,10 +11,47 @@ import { Router } from '@angular/router';
 export class NavMenuComponent {
   constructor(private router: Router) { }
 
+  showCategoriesStatement : boolean = false;
+  searchObject = new Search();
+
   ngOnInit() {
   }
 
   search(query : string) {
+    console.log(this.searchObject.subject);
     this.router.navigate([''], {queryParams: {s: query}});
+  }
+
+  showCategories() {
+    this.showCategoriesStatement = !this.showCategoriesStatement;
+  }
+
+  redirectToHome() {
+    this.router.navigateByUrl("/");
+  }
+
+  redirectToLogin() {
+    this.router.navigateByUrl("/login");
+  }
+
+  redirectToSignUp() {
+    this.router.navigateByUrl("/signup");
+  }
+
+  hasAccessToken() {
+    console.log(localStorage.getItem("access_token"));
+    
+    if ((localStorage.getItem("access_token") === "undefined")
+        || localStorage.getItem("access_token") === null) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  showAccessToken() {
+    console.log(localStorage.getItem("access_token"));
+    localStorage.removeItem("access_token");
   }
 }
