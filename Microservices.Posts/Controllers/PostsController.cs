24fd6b229cf90a8b2f41126b382931c2ab7dbf72
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-16 14:13:18 +0200

    init

---
+++ Microservices.Posts/Controllers/PostsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Posts.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+
+        [HttpPost]
+        public void Create()
+        {
+
+        }
+
+        [HttpPut]
+        public void Update()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+    }
+}
