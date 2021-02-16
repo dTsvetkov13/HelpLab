ab4812197a33e3ccf06acaa206ce109317ce6794
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-16 14:57:08 +0200

    Controller added

---
+++ Microservices.Users/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservices.Users.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+
+        }
+
+        [HttpPost]
+        public void Create()
+        {
+
+        }
+
+        [HttpPut]
+        public void Update()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+    }
+}
