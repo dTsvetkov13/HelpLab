fb377be499bd5121b9ec526547090f8dcce8936f
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-18 19:12:34 +0200

    EventMessage added

---
+++ Microservices.Models/Messages/EventMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.Models.Messages
+{
+    public class EventMessage
+    {
+        public EventMessage(EventsEnum EventObject, string userId)
+        {
+            Event = EventObject;
+            UserId = userId;
+        }
+
+        public EventsEnum Event { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
