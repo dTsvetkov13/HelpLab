da30d38a5d19a6a54083b8b64dd03765e718ac58
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-22 18:06:49 +0200

    renamed

--- APIGateway/Models/InputModels/CreatePostInputModel.cs
+++ APIGateway/Models/InputModels/CreatePostInputModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace APIGateway.Models.InputModels
 {
-    public class PostInputModel
+    public class CreatePostInputModel
     {
         [Required]
         public string Title { get; set; }
