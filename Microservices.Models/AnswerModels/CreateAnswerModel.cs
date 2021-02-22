757a3b0554a0ad5a7b4d257d464aa864b76e2a68
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-22 18:25:36 +0200

    init

---
+++ Microservices.Models/AnswerModels/CreateAnswerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.Models.AnswerModels
+{
+    public class CreateAnswerModel
+    {
+        public string Text { get; set; }
+
+        public DateTime PublishedAt { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public string PostId { get; set; }
+
+        public string AnswerId { get; set; }
+    }
+}
