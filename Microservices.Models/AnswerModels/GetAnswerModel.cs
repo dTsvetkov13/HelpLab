a152d20f89cda379b4772b7a8d92387a92305061
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-23 20:08:52 +0200

    updated

---
+++ Microservices.Models/AnswerModels/GetAnswerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.Models.AnswerModels
+{
+    public class GetAnswerModel
+    {
+        public string Text { get; set; }
+
+        public DateTime PublishedAt { get; set; }
+
+        public DateTime LastEditedAt { get; set; }
+
+        public string AuthorId { get; set; }
+
+        //TODO: add attribute
+        public Guid PostId { get; set; }
+
+        public Guid AnswerId { get; set; }
+    }
+}
