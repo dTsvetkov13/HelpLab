044be3dd2a5151816b688f5328e766410ef1ac9e
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-23 20:08:20 +0200

    Models for response added

---
+++ Microservices.Models/Common/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.Models.Common
+{
+    public class Response
+    {
+        public Statuses Status { set; get; }
+
+        public string Error { set; get; }
+    }
+}
---
+++ Microservices.Models/Common/Statuses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.Models.Common
+{
+    public enum Statuses
+    {
+        Ok,
+        InvalidData,
+        ServerError
+    }
+}
