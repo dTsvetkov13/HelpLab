40fa3ea42f8535309bf5957e250079c4e07e4f9b
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-23 20:17:54 +0200

    Messages for answers added

--- Microservices.EventBus/Common/MessagesEnum.cs
+++ Microservices.EventBus/Common/MessagesEnum.cs
@@ -9,5 +9,9 @@ namespace Microservices.EventBus
         public const string PostsRoute = "posts";
         public const string PostsCreatedRoute = PostsRoute + ".created";
         public const string PostsDeletedRoute = PostsRoute + ".deleted";
+
+        public const string AnswersRoute = "answers";
+        public const string AnswersCreatedRoute = AnswersRoute + ".created";
+        public const string AnswersDeletedRoute = AnswersRoute + ".deleted";
     }
 }
