0d20e95c760fb48b4fb3aac254c14e7e9db97722
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-23 20:12:00 +0200

    init

---
+++ Microservices.Models/UserModels/GetUserResponse.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.Models.UserModels
+{
+    public class GetUserResponse : IdentityUser
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int PostsCount { get; set; }
+
+        public int AnswersCount { get; set; }
+    }
+}
