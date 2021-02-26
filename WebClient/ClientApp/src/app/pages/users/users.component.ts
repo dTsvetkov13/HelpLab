df9ddeb553365e26195eccd0a71cdc76e4790002
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-26 20:08:39 +0200

    Show data depending on variables

--- WebClient/ClientApp/src/app/pages/users/users.component.ts
+++ WebClient/ClientApp/src/app/pages/users/users.component.ts
@@ -1,22 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Post } from '../../models/post';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  isAnswersButtonPressed: boolean = false;
+  isPostsButtonPressed: boolean = false;
+  posts: Post[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.isPostsButtonPressed = true;
+
+    this.posts[0] = new Post("Title", "Descrip", "12.3.2002", 2, [])
+    this.posts[1] = new Post("Title", "Descrip", "12.3.2002", 2, [])
+
     const url = window.location.href;
 
     const params = url.split("/");
 
-
-
     console.log(params[params.length - 1]);
   }
 
+  showAnswers() {
+    this.isPostsButtonPressed = false;
+    this.isAnswersButtonPressed = true;
+  }
+
+  showPosts() {
+    this.isPostsButtonPressed = true;
+    this.isAnswersButtonPressed = false;
+  }
 }
