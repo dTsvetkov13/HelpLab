eef1ac92be6219c325a00641326057a9084f3c8a
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-02-26 20:07:23 +0200

    Add new properties

--- WebClient/ClientApp/src/app/models/post.ts
+++ WebClient/ClientApp/src/app/models/post.ts
@@ -1,8 +1,11 @@
 export class Post {
 
     constructor(
-      public question: string,
+      public title: string,
       public description: string,
+      public publishedAt: string,
+      public answersCount: number,
+      public categories: string[]
     ) {  }
   
   }
