b96382a2cbe63b596f73ed9b529156ab9331e3f1
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-03-02 10:43:14 +0200

    Get with Authorize header add

--- WebClient/ClientApp/src/app/services/web-requests.service.ts
+++ WebClient/ClientApp/src/app/services/web-requests.service.ts
@@ -9,11 +9,15 @@ export class WebRequestsService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = "https://localhost:44395";
+    this.ROOT_URL = "https://localhost:44330";
   }
 
   get(url: string) {
     return this.http.get(`${this.ROOT_URL}/${url}`, { observe: 'response', responseType: 'text' });
+  }
+
+  getAuthorized(url: string) {
+    return this.http.get(`${this.ROOT_URL}/${url}`, { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('access_token') }, observe: 'response', responseType: 'text' });
   }
   
   //Authorized post
