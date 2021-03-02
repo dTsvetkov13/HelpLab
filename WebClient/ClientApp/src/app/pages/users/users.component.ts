d7ba16b1115e0c1e0f3d96e2c195278cacf5c3d3
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-03-02 10:39:09 +0200

    Implement '/me' endpoint

--- WebClient/ClientApp/src/app/pages/users/users.component.ts
+++ WebClient/ClientApp/src/app/pages/users/users.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Post } from '../../models/post';
+import { Answer } from '../../models/answer';
+import { WebRequestsService } from '../../services/web-requests.service';
+import { HttpResponse } from '@angular/common/http';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-users',
@@ -10,23 +14,48 @@ import { Post } from '../../models/post';
 export class UsersComponent implements OnInit {
   isAnswersButtonPressed: boolean = false;
   isPostsButtonPressed: boolean = false;
+  isUserLoaded: boolean = false;
   posts: Post[] = [];
+  answers: Answer[] = [];
+  user: User;
 
-  constructor() { }
+  constructor(private webRequest: WebRequestsService) { }
 
   ngOnInit() {
     this.isPostsButtonPressed = true;
-
-    this.posts[0] = new Post("Title", "Descrip", "12.3.2002", 2, [])
-    this.posts[1] = new Post("Title", "Descrip", "12.3.2002", 2, [])
 
     const url = window.location.href;
 
     const params = url.split("/");
 
-    console.log(params[params.length - 1]);
+    let id = params[params.length - 1];
+    let data;
+
+    if (id == "me") {
+      this.webRequest.getAuthorized("api/users").subscribe((res: HttpResponse<any>) => {
+        console.log("Success login" + res.status);
+        data = JSON.parse(res.body);
+        console.log(data);
+        this.user = new User(data["name"], data["surname"], parseInt(data["answersCount"]), parseInt(data["postsCount"]))
+        this.isUserLoaded = true;
+      },
+        error => {
+          console.log("oops", error);
+
+        });
+    } else {
+      data = this.webRequest.get("api/users/" + id);
+      this.user = new User(data["name"], data["surname"], parseInt(data["answersCount"]), parseInt(data["postsCount"]))
+      this.isUserLoaded = true;
+    }
+
+    this.posts[0] = new Post("Title", "Descrip", "12.3.2002", 2, [])
+    this.posts[1] = new Post("Title", "Descrip", "12.3.2002", 2, [])
+
+    this.answers[0] = new Answer("Title", "12.23.2001", 1, null);
   }
 
+    
   showAnswers() {
     this.isPostsButtonPressed = false;
     this.isAnswersButtonPressed = true;
